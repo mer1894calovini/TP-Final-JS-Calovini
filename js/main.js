7b750f6e070f2bd302139fa2f0b2d845a08a9846
mer1984calovini <m_erchu24@live.com>
2023-03-23 12:06:11 -0300

    modificaciones

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-class Prenda {
+ class Prenda {
     constructor (id, nombre, precio,img){
         this.id = id;
         this.nombre = nombre;
@@ -14,30 +14,60 @@ const short = new Prenda(4, "short", 3000, "img/short.jpg");
 const zapatillas = new Prenda(5, "zapatillas", 7000, "img/zapatillas.jpg");
 
 const prendas = [calza, campera, gorra, short, zapatillas];
-console.log(prendas)   
+console.log(prendas)    
 
     
 let carrito = [];
 
 carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+let Prendas
+async function fetchPrendas() {
 
-const listadoDePrendas = "./js/prendas.json"
+    try {
 
-fetch(listadoDePrendas)
-    .then(response  => response.jason())
-    .then(data  => {
-        prendas = data;
-        importarProductos(data);
-    })
-    .catch(error => console.log(error));
+        const response = await fetch(listadoDePrendas);
 
+        if (!response.ok) {
 
-if(localStorage.getItem("carrito")){
-    carrito = JSON.parse(localStorage.getItem("carrito"));
+        throw new Error(`HTTP error! status: ${response.status}`);
+
+        }
+
+        const data = await response.json();
+
+        return data;
+
+    } catch (error) {
+
+        console.error(error);
+
+    }
+
 }
 
+async function importarPrendas() {
 
-const listaDePrendas = document.getElementById("listaDePrendas");
+    try {
+
+        const data = await fetchPrendas();
+
+          prendas = data; // Use spread operator to append new data to prendas array
+
+        mostrarPrendas(prendas);
+
+    } catch (error) {
+
+        console.error(error);
+
+        }
+
+}
+
+importarPrendas();
+
+
+
+
 
 const mostrarPrendas = () => {
     prendas.forEach(prenda  => {
@@ -50,8 +80,7 @@ const mostrarPrendas = () => {
                             <h5> ${prenda.nombre} </h5>
                             <p> ${prenda.precio} <p>
                             <button class = "btn  colorButton" id="boton${prenda.id}">Agregar al carrito</button>
-                            
-                            </div>
+                        </div>
                     </div>
                     `
         listaDePrendas.appendChild(card);  
@@ -94,22 +123,22 @@ const mostrarCarrito =() => {
                                 <h5> ${prenda.nombre} </h5>
                                 <p> ${prenda.precio} </p>
                                 <p> ${prenda.cantidad} </p> 
-                                <button class = "btn  colorButton" id="mas${prenda.cantidad}" >+</button>
-                                <button class = "btn  colorButton" id="menos${prenda.cantidad}" >-</button>
+                                <button class = "btn  colorButton" id="mas${prenda.id}" >+</button>
+                                <button class = "btn  colorButton" id="menos${prenda.id}" >-</button>
                                 <button class = "btn  colorButton" id="eliminar${prenda.id}" >Eliminar</button>
                             </div>
                         </div>
                         `
         contenedorCarrito.appendChild(card);
         
-        const botonMenos = document.getElementById(`menos${prenda.cantidad}`);
+        const botonMenos = document.getElementById(`menos${prenda.id}`);
         botonMenos.addEventListener("click", () => {
-            disminuirPrenda(prenda.cantidad); 
+            modificarCantidad(prenda.id, "restar"); 
         })
         
-        const botonMas = document.getElementById(`mas${prenda.cantidad}`);
+        const botonMas = document.getElementById(`mas${prenda.id}`);
         botonMas.addEventListener("click", () => {
-            disminuirPrenda(prenda.cantidad); 
+            modificarCantidad(prenda.id, "sumar"); 
         }) 
         
             const boton = document.getElementById(`eliminar${prenda.id}`);
@@ -119,7 +148,7 @@ const mostrarCarrito =() => {
         
     
     montoTotal();
-}) }
+    }) }
 
 const eliminarDelCarrito = (id)  => {
     const prenda = carrito.find(prenda => prenda.id === id);
@@ -131,22 +160,15 @@ const eliminarDelCarrito = (id)  => {
 
 
 
-const disminuirPrenda = (id) => {
-    const prenda = carrito.find((prenda) => producto.id ===id);
-    prenda.cantidad --;
-    if(prenda.cantidad === 0){
-        eliminarDelCarrito();
-    }else{
-        localStorage.setItem("carrito", JSON.stringify(carrito));
-    }  
-    mostrarCarrito();
-}
-
-const masPrenda = (id)  => {
-    const prenda = carrito.find((prenda) => producto.id ===id );
-    prenda.cantidad ++;
-    localStorage.setItem("carrito" , JSON.stringify(carrito));
-    mostrarCarrito();
+function modificarCantidad(id, accion) {
+    const prenda = carrito.find(prenda => prenda.id === id);
+    if (prenda) {
+        if (accion === "sumar") {
+        prenda.cantidad += 1;
+} else if (accion === "restar" && prenda.cantidad > 1) {
+    prenda.cantidad -= 1;
+}}
+mostrarCarrito();
 }
     
 
